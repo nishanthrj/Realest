6c04af6b8d830520e116949b6c14efc9d2b7411e
Nishanth R J <mycloudiswhite@gmail.com>
2020-11-19 16:26:05 +0530

    Added Team and MVP to About Page

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from listings.models import Listing
-from realtors.models import Realtors
+from realtors.models import Realtor
 
 def index(response):
 	listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:2]
@@ -12,5 +12,11 @@ def index(response):
 	return render(response, 'pages/index.html', context)
 
 def about(response):
+	realtors = Realtor.objects.all()
+	mvps = Realtor.objects.all().filter(is_mvp=True)
+	context = {
+		'realtors': realtors,
+		'mvps': mvps
+	}
 
-	return render(response, 'pages/about.html')
+	return render(response, 'pages/about.html', context)
