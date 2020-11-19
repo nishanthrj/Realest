5d98f2b64ed68d1f9936d7f0d8cd6b7b29989406
Nishanth R J <mycloudiswhite@gmail.com>
2020-11-19 16:03:48 +0530

    Added Latest Listings to Index Page

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from listings.models import Listing
+from realtors.models import Realtors
 
 def index(response):
-		return render(response, 'pages/index.html')
+	listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:2]
+	context = {
+		'listings': listings
+	}
 
-
+	return render(response, 'pages/index.html', context)
 
 def about(response):
+
 	return render(response, 'pages/about.html')
