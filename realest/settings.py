c5ecdb204c1be3c755d34bf16259f02c5538cb59
Nishanth R J <mycloudiswhite@gmail.com>
2020-11-20 12:58:47 +0530

    Created Register and Login Page

--- realest/settings.py
+++ realest/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts.apps.AccountsConfig',
     'realtors.apps.RealtorsConfig',
     'listings.apps.ListingsConfig',
     'pages.apps.PagesConfig',
@@ -133,3 +134,9 @@ STATICFILES_DIRS = [
 # Media Files Settings
 MEDIA_ROOT = Path.joinpath(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+# Messages
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger'
+}
