9750ff8a5f8e7069cc93e87a9455fbec137aba27
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2022-11-16 12:45:50 +0530

    Configured  Templates

--- pages/views.py
+++ pages/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(response):
-	return HttpResponse('<h1>Hello World</h1>')
+		return render(response, 'pages/index.html')
+
+
+
+def about(response):
+	return render(response, 'pages/about.html')
